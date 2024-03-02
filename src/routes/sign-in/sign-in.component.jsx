ffcadb2e396731c4ff0d8d432e1b0ae4f5714db5
Soumy Oza <ozasoumy2000@gmail.com>
2024-03-02 18:35:48 +0530

    Updated code for today

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,18 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth, signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 
 
 const SignIn = () => {
 
     const logGoogleUsers = async () => {
-        const response = await signInWithGooglePopup();
-        console.log(response);
-    }
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    };
     return (
         <div>
-            <h1> Sign In </h1>
+            <h1> SIGN IN </h1>
             <button onClick={logGoogleUsers}>
                 Sign in with Google popup
             </button>
-            <h1> SIGN IN </h1>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,7 +23,24 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
 
-const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userData = await getDoc(userDocRef);
+
+    if(!userData.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch(error) {
+            console.log("Error message", error.message);
+        }
+    }
+
+    return userDocRef;
 }
