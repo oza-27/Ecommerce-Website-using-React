6f13d07f84c9e739a7d2bea76b8cf69e2a22211b
Soumy Oza <ozasoumy2000@gmail.com>
2024-03-02 18:04:41 +0530

    Added firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
+
 const SignIn = () => {
+
+    const logGoogleUsers = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
     return (
         <div>
             <h1> Sign In </h1>
+            <button onClick={logGoogleUsers}>
+                Sign in with Google popup
+            </button>
         </div>
     )
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyCYA_xJ-Qy79xzlO96Lq8AgsGEPtnE1cWk",
+    authDomain: "ecommerce-clothingdb-1e9ef.firebaseapp.com",
+    projectId: "ecommerce-clothingdb-1e9ef",
+    storageBucket: "ecommerce-clothingdb-1e9ef.appspot.com",
+    messagingSenderId: "411790383554",
+    appId: "1:411790383554:web:3b4df1c1e50ba9eee31e5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userData = await getDoc(userDocRef);
+}
